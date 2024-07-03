37950378ba66ee7fcc106dc2c9b31a3624bac299
SHY-89 <tjduwkrn@gmail.com>
2024-07-03 12:01:24 +0900

    추가기능 부분 추가

--- main.py
+++ main.py
@@ -25,3 +25,59 @@ members = []
 posts = []
 
 # TODO : 코드 구현이 필요합니다.
+
+
+# commnd control code
+print("════════════ ೋღ 🌺 ღೋ ════════════")
+print("\t퍼스트 코팅 : 회원과 게시물을 관리")
+print("════════════ ೋღ 🌺 ღೋ ════════════")
+main_pointer = input("회원관리(1)/게시물관리(2)/종료(3) : ")
+while main_pointer != '3':
+    print_content =[
+        ['회원 이름:','회원 아이디:','회원 비밀번호:'],
+        ['게시물 제목:','게시물 내용:','작성자:']
+    ]
+    member_info = []
+    post_info = []
+    types = None
+    if main_pointer == '1':
+        types = '회원'
+    elif main_pointer == '2':
+        types = '게시물'
+    elif main_pointer != '3':
+        print("잘 못 입력하였습니다. 다시 입력해주세요.")
+        main_pointer = input("회원관리(1)/게시물관리(2)/종료(3) : ")
+        continue
+
+    sub_pointer = input(f"{types} 생성(1)/{types} 목록(2)/{types} 이름 검색(3)/{types} 아이디 검색(4)/메인이동(5) : ")
+    while sub_pointer != '5':
+        if main_pointer == '1':
+            all_print = member_info
+        else:
+            all_print = post_info
+        if sub_pointer == '1':
+            a = input(f"{print_content[int(main_pointer)-1][0]}")
+            b = input(f"{print_content[int(main_pointer)-1][1]}")
+            c = input(f"{print_content[int(main_pointer)-1][2]}")
+            if main_pointer == '1':
+                member_info.append(Member(a,b,c))
+            else:
+                post_info.append(Post(a, b, c))
+
+        elif sub_pointer == '2':
+            for value in all_print:
+                value.display()
+        elif sub_pointer == '3':
+            serch_value = input(f"찾는 {print_content[int(main_pointer)-1][0]}")
+            for value in all_print:
+                if (main_pointer == '1' and serch_value in value.name) or (main_pointer == '2' and serch_value in value.title):
+                    value.display()
+        elif sub_pointer == '4':
+            serch_value = input(f"찾는 {print_content[int(main_pointer) - 1][1]}")
+            for value in all_print:
+                if (main_pointer == '1' and serch_value in value.username) or (main_pointer == '2' and serch_value in value.content):
+                    value.display()
+        elif sub_pointer != '5':
+            print("잘 못 입력하였습니다. 다시 입력해주세요.")
+        sub_pointer = input(f"{types} 생성(1)/{types} 목록(2)/{types} 이름 검색(3)/{types} 아이디 검색(4)/메인이동(5) : ")
+    main_pointer = input("회원관리(1)/게시물관리(2)/종료(3) : ")
