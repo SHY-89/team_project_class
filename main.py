e9c77b1c20e07c5cb5977d387e92cb3c1f5c75c2
rabongee <rmarkdehdals@gmail.com>
2024-07-03 10:41:18 +0900

    멤버 클래스 구현

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # ----- 코드 정의 ------
 class Member:
-    # TODO : 코드 구현이 필요합니다.
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = username
+        self.password = password
 
     def display(self):
-        # TODO : 코드 구현이 필요합니다.
-        pass
+        print(f"이름 : {self.name}\n아이디 : {self.username}")
 
 
 # Post 클래스
