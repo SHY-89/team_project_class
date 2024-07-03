8efdce37668341d2309b07d188330ae8beeb6d34
zhddkf <107788626+zhddkf@users.noreply.github.com>
2024-07-03 10:30:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ class Member:
         pass
 
 
+# Post 클래스
 class Post:
-    # TODO : 코드 구현이 필요합니다.
-    pass
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
+
+    def display(self):
+        print(f'제목: {self.title} \n내용: {self.content}')
 
 
 # ----- 코드 실행 ------
